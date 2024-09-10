fe9ad85cca47e198546d085cd683a919077f3f6c
Anas-github-acc <anas.ahamad955@gmail.com>
2024-09-10 19:47:14 +0530

    pushing 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
+import React from 'react';
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <Image src="/logo.png" alt="logo" width={100} height={100} />
     </div>
   );
 }
